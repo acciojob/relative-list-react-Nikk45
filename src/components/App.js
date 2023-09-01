2df87b0c2e08b31fbf92ca76a0a8918d90e4fc57
Nikk45 <nicksin36@gmail.com>
2023-09-01 14:46:07 +0530

    submit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+const relatives = [
+  "Parents",
+  "Siblings",
+  "Grandparents",
+  "Aunts and Uncles",
+  "Cousins",
+  "Nieces and Nephews",
+  "Spouse and In-Laws",
+  "Children and Grandchildren",
+  "Godparents",
+  "Family Friends",
+  "Close Relatives living abroad",
+  "Great-Grandparents",
+  "Great Aunts and Great Uncles",
+  "Step-Relatives",
+  "Adopted Relatives"
+]
+
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol>
+               {
+                relatives.map((relative,index)=>
+                  <li key={`relativeListItem${index+1}`}>{relative}</li>
+                )
+               }
+               </ol>
     </div>
   )
 }
